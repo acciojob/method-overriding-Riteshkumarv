22d64144ec2bb6ede53c5f82695cd70d865aea36
ritesh kumar <Ritesh74461@gmail.com>
2023-03-14 14:42:39 +0530

    Assignment submit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String []args){
+        A obj = new B();
+        System.out.print(obj.meth());
+    }
+}
+class A{
+   static String meth(){
+        return "Invoked method from class A";
+    }
+}
+class B extends A{
+    static String meth(){
+        return "Invoked method from class B";
+    }
 }
